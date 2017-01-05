9e559db0adf6e947bb99c3778f074c50bc2457ca
Peter M Taylor <p.taylor24@yahoo.com.au>
2017-01-06 08:00:30 +1100

    Start section one tests and start coding the file.

--- CodeAudit.Tests.ps1
+++ CodeAudit.Tests.ps1
@@ -8,3 +8,15 @@ It "does something useful" {
         $true | Should Be $false
     }
 }
+
+Describe "Section 1 - Folder selection" -tag "Section 1" {
+
+It "Able to recieve a folder selection" {
+         $true | Should Be $false
+     }
+}
+
+It "Reject folder selection if none supplied" {
+         $true | Should Be $false
+     }
+}
