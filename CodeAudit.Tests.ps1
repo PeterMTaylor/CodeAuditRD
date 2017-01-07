b82813a884d2ef85aeaf0d7fe73ac8138b22ec57
Peter M Taylor <p.taylor24@yahoo.com.au>
2017-01-07 13:03:42 +1100

    Reset Pester script to original format to start again.

--- CodeAudit.Tests.ps1
+++ CodeAudit.Tests.ps1
@@ -2,21 +2,8 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
-Describe "CodeAudit as Module" -tag "Module" {
-
-It "does something useful" {
+Describe "CodeAudit" {
+    It "does something useful" {
         $true | Should Be $false
     }
-}
-
-Describe "Section 1 - Folder selection" -tag "Section 1" {
-
-It "Able to recieve a folder selection" {
-         $true | Should Be $false
-     }
-
-
-It "Reject folder selection if none supplied" {
-         $true | Should Be $false
-     }
 }
