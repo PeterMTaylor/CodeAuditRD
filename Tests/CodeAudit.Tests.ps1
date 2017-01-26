094432a37bf09373444fd74d72e6720c12d9a17e
Peter M Taylor <PeterMTaylor@users.noreply.github.com>
2017-01-26 12:21:52 +1100

    Update CodeAudit.Tests.ps1
    Correct translation of characters from AppVeyor demo as per AppVeyor Log on 1,0.58 â€˜ to ' or ".

--- Tests/CodeAudit.Tests.ps1
+++ Tests/CodeAudit.Tests.ps1
@@ -1,16 +1,16 @@
 #---------------------------------# 
 # PSScriptAnalyzer tests          # 
 #---------------------------------# 
-$Scripts = Get-ChildItem “$PSScriptRoot\..\” -Filter ‘*.ps1’ -Recurse | Where-Object {$_.name -NotMatch ‘Tests.ps1’}
-$Modules = Get-ChildItem “$PSScriptRoot\..\” -Filter ‘*.psm1’ -Recurse
+$Scripts = Get-ChildItem "$PSScriptRoot\..\" -Filter '*.ps1' -Recurse | Where-Object {$_.name -NotMatch 'Tests.ps1'}
+$Modules = Get-ChildItem "$PSScriptRoot\..\" -Filter '*.psm1' -Recurse
 $Rules   = Get-ScriptAnalyzerRule
 
 if ($Modules.count -gt 0) {
-  Describe ‘Testing all Modules against default PSScriptAnalyzer rule-set’ {
+  Describe 'Testing all Modules against default PSScriptAnalyzer rule-set' {
     foreach ($module in $modules) {
-      Context “Testing Module '$($module.FullName)'” {
+      Context "Testing Module '$($module.FullName)'" {
         foreach ($rule in $rules) {
-          It “passes the PSScriptAnalyzer Rule $rule“ {
+          It "passes the PSScriptAnalyzer Rule $rule" {
             (Invoke-ScriptAnalyzer -Path $module.FullName -IncludeRule $rule.RuleName ).Count | Should Be 0
           }
         }
@@ -19,11 +19,11 @@ if ($Modules.count -gt 0) {
   }
 }
 if ($Scripts.count -gt 0) {
-  Describe ‘Testing all Script against default PSScriptAnalyzer rule-set’ {
+  Describe 'Testing all Script against default PSScriptAnalyzer rule-set' {
     foreach ($Script in $scripts) {
-      Context “Testing Script '$($script.FullName)'” {
+      Context "Testing Script '$($script.FullName)'" {
         foreach ($rule in $rules) {
-          It “passes the PSScriptAnalyzer Rule $rule“ {
+          It "passes the PSScriptAnalyzer Rule $rule" {
             if (-not ($module.BaseName -match 'AppVeyor') -and -not ($rule.Rulename -eq 'PSAvoidUsingWriteHost') ) {
               (Invoke-ScriptAnalyzer -Path $script.FullName -IncludeRule $rule.RuleName ).Count | Should Be 0
             }
@@ -41,7 +41,7 @@ if ($Scripts.count -gt 0) {
 $PSVersion    = $PSVersionTable.PSVersion.Major
 $AppVeyorDemo = "$PSScriptRoot\..\AppVeyorDemo.psm1"
 
-Describe "AppVeyorDemo PS$PSVersion" {
+Describe "CodeAudit PS$PSVersion" {
     Copy-Item $AppVeyorDemo TestDrive:\script.ps1 -Force
     Mock Export-ModuleMember {return $true}
     . 'TestDrive:\script.ps1'
