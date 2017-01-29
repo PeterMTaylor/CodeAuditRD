3a4a4ddf3a0163de9b1619167000b387b0696981
Peter M Taylor <PeterMTaylor@users.noreply.github.com>
2017-01-29 14:07:06 +1100

    Update AppVeyorDeploy.ps1
    Not required to deploy into Powershell

--- AppVeyor/AppVeyorDeploy.ps1
+++ AppVeyor/AppVeyorDeploy.ps1
@@ -24,6 +24,6 @@ $ModulePath = Split-Path $pwd
 Write-Host "Adding $ModulePath to 'psmodulepath' PATH variable"
 $env:psmodulepath = $env:psmodulepath + ';' + $ModulePath
 
-Write-Host 'Publishing module to Powershell Gallery'
+#Write-Host 'Publishing module to Powershell Gallery'
 #Uncomment the below line, make sure you set the variables in appveyor.yml
 #Publish-Module -Name $env:ModuleName -NuGetApiKey $env:NuGetApiKey
