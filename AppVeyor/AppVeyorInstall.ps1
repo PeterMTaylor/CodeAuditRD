40290b47aa9c6bf06e31a1691803bb64d0ead91d
Peter M Taylor <PeterMTaylor@users.noreply.github.com>
2017-02-03 07:47:54 +1100

    Update AppVeyorInstall.ps1
    Introducing FOrmat Pester

--- AppVeyor/AppVeyorInstall.ps1
+++ AppVeyor/AppVeyorInstall.ps1
@@ -17,6 +17,13 @@ Write-Host 'Installing Pester'
 Install-Module -Name Pester -Repository PSGallery -Force
 Get-Module -ListAvailable -Name Pester | Format-List Name,Version,ModuleBase
 
+#---------------------------------#
+#  Install Format-Pester          #
+#---------------------------------#
+Write-Host 'Installing Format-Pester'
+Install-Module -Name Format-Pester -Respository PSGallery -Force
+Get-Module -ListAvailble -Name Format-Pester | Format-List Name,Version,ModuleBase
+
 #---------------------------------# 
 # Install PSScriptAnalyzer        # 
 #---------------------------------# 
