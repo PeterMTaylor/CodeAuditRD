2536a555f6b4897b414c6b08836b07980f886e14
Peter M Taylor <PeterMTaylor@users.noreply.github.com>
2017-02-13 21:44:37 +1100

    Update AppVeyorInstall.ps1
    Resolve conflicts

--- AppVeyor/AppVeyorInstall.ps1
+++ AppVeyor/AppVeyorInstall.ps1
@@ -6,7 +6,8 @@ Write-Host 'Running AppVeyor install script' -ForegroundColor Yellow
 #---------------------------------#
 # Powershell status               #
 #---------------------------------#
-Write-Host "Installed Powershell '$PSVersionTable'"
+Write-Host "Installed Powershell PSVersion: $($PSVersionTable.PSVersion), build: $($PSVersionTable.BuildVersion), clr version: $($PSVersionTable.ClrVersion)"
+Write-Host "Host name: $($Host.Name)"
 
 #---------------------------------# 
 # Install NuGet                   # 
@@ -21,6 +22,20 @@ Write-Host "Installed NuGet version '$($pkg.version)'"
 Write-Host 'Installing Pester'
 Install-Module -Name Pester -Repository PSGallery -Force
 Get-Module -ListAvailable -Name Pester | Format-List Name,Version,ModuleBase
+
+#---------------------------------#
+#  Install Pscribo                #
+#---------------------------------#
+Write-Host 'Installing PScribo'
+Install-Module -Name PScribo -Repository PSGallery -Force
+Get-Module -ListAvailable -Name PScribo | Format-List Name,Version,ModuleBase
+
+#---------------------------------#
+#  Install Format-Pester         #
+#---------------------------------#
+Write-Host 'Installing Format-Pester'
+Install-Module -Name Format-Pester -Force
+Get-Module -ListAvailable -Name Format-Pester | Format-List Name,Version,ModuleBase
 
 #---------------------------------# 
 # Install PSScriptAnalyzer        # 
