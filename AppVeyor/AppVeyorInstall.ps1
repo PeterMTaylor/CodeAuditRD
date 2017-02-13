39e6ba3fd1ad9bf40af718c759d69d8b3a885559
Peter M Taylor <PeterMTaylor@users.noreply.github.com>
2017-02-14 08:24:06 +1100

    Update AppVeyorInstall.ps1
    Pscribo and format-pester not required in this branch

--- AppVeyor/AppVeyorInstall.ps1
+++ AppVeyor/AppVeyorInstall.ps1
@@ -23,20 +23,6 @@ Write-Host 'Installing Pester'
 Install-Module -Name Pester -Repository PSGallery -Force
 Get-Module -ListAvailable -Name Pester | Format-List Name,Version,ModuleBase
 
-#---------------------------------#
-#  Install Pscribo                #
-#---------------------------------#
-Write-Host 'Installing PScribo'
-Install-Module -Name PScribo -Repository PSGallery -Force
-Get-Module -ListAvailable -Name PScribo | Format-List Name,Version,ModuleBase
-
-#---------------------------------#
-#  Install Format-Pester         #
-#---------------------------------#
-Write-Host 'Installing Format-Pester'
-Install-Module -Name Format-Pester -Force
-Get-Module -ListAvailable -Name Format-Pester | Format-List Name,Version,ModuleBase
-
 #---------------------------------# 
 # Install PSScriptAnalyzer        # 
 #---------------------------------# 
