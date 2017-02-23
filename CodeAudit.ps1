c13371cb9e8ea38da97933e746f62bd545e1573b
Peter M Taylor <PeterMTaylor@users.noreply.github.com>
2017-02-23 20:06:56 +1100

    Update CodeAudit.ps1
    Initial comments to check the test does confirm required comments and function notes.

--- CodeAudit.ps1
+++ CodeAudit.ps1
@@ -1,9 +1,29 @@
 function Get-SomeInt
+<#
+    .SYNOPSIS
+        Get-SomeInt is a test. This comment is a test.
+    .NOTES
+        This is an internal function and should not be called directly.
+    .LINK
+        This is derived from my work.
+    .OUTPUTS
+        Does a integer test.
+#>
 {
     Return Get-Random -Minimum 1 -Maximum 99
 }
 
 function CodeAudit
+<#
+    .SYNOPSIS
+        Eventually I would have this done!
+    .NOTES
+        Looking forward then to make a Powershell RDUtility for checking configurations.
+    .LINK
+        To rubberduck
+    .OUTPUTS
+        something like that
+#>
 {
      Return write-output 'Hello World!' 
 }
