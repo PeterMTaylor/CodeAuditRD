ce8f7a30ceb161d299933d78c54b3eddaa970928
Peter M Taylor <PeterMTaylor@users.noreply.github.com>
2017-03-01 22:50:20 +1100

    Update CodeAudit.ps1

--- CodeAudit.ps1
+++ CodeAudit.ps1
@@ -3,10 +3,10 @@ function CodeAudit
     .SYNOPSIS
         CodeAudit is a Powershell script to check the Github respository against specified domain using Https requests for 
         auditing purposes of software code to validate software licence.
-    .DESCRIPTION
+    .Description
          need to describe the entire process in step form here.
    .NOTES
-      Written By Peter M Taylor. 
+      Author Peter M Taylor. 
       Located at Peter M Taylor on Github, github.com/PeterMTaylor
       Twitter @peterlearning24
 #>
