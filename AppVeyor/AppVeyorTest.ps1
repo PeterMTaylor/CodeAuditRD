c62fb3c497f9941ae7c79f8ee8e9be71f679b091
Peter M Taylor <PeterMTaylor@users.noreply.github.com>
2017-03-22 22:11:04 +1100

    Update AppVeyorTest.ps1
    Attempt to resolve Systems.Web.Client by changing ensuring the export is [XML] forced by Type.

--- AppVeyor/AppVeyorTest.ps1
+++ AppVeyor/AppVeyorTest.ps1
@@ -8,10 +8,16 @@ Write-Host "Current working directory: $pwd"
 # Run Pester Tests                # 
 #---------------------------------# 
 $testResultsFile = '.\TestsResults.xml'
-$res             = Invoke-Pester -Script .\CodeAudit.Tests.ps1 -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
+$res             = Invoke-Pester -Script .\CodeAudit.Tests.ps1 -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru | 
+      Export-Clixml $testResultsFile
+
+Write-Host 'Completed Invoke-Pester. Saving results.'
+    [xml]$content = Get-Content $testResultsFile
+    $content.'test-results'.'test-suite'.type = "Powershell"
+    $content.Save($testResultsFile)
 
 Write-Host 'Uploading results'
-(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
+#(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
 
 if (Test-Path $testResultsFile) {
     (New-Object 'Systems.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$(env:APPVEYOR_JOB_ID)", $testResultsFile)
