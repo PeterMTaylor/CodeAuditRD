3b7c232544f94a94b981c5e1b69662f409f5fb0a
Peter M Taylor <PeterMTaylor@users.noreply.github.com>
2017-04-06 17:55:49 +1000

    Update CodeAuditRD.psm1

--- CodeAuditRD.psm1
+++ CodeAuditRD.psm1
@@ -1,3 +1,3 @@
 ## Import the CodeAudit.ps1 file. This permits loading of the module's
 ## functions for unit testing, without having to unload/load the whole module.
-. (Join-Path -Path (Split-Path -Path $PSCommandPath) -ChildPath CodeAudit.ps1);
+. (Join-Path -Path (Split-Path -Path $PSCommandPath) -ChildPath CodeAuditRD.ps1);
