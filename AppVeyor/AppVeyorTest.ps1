da9ce5982794c9e6c6fd3a7ea1fe2a81cf5fd809
Peter M Taylor <PeterMTaylor@users.noreply.github.com>
2017-04-27 16:41:48 +1000

    Update AppVeyorTest.ps1
    Remove test result variable and use as given

--- AppVeyor/AppVeyorTest.ps1
+++ AppVeyor/AppVeyorTest.ps1
@@ -8,22 +8,22 @@ Write-Host "Current working directory: $pwd"
 # Run Pester Tests                # 
 #---------------------------------# 
 Invoke-Pester -Script .\CodeAuditRD.Tests.ps1 -OutputFile .\Test-Result.xml -OutputFormat NUnitXml -PassThru | Export-Clixml -Path .\Test-Result.xml
-# format pest here!
-Write-Host 'Completed Invoke-Pester. Saving results.'
-    [xml]$content = Get-Content $testResultsFile
-    $content.'test-results'.'test-suite'.type = "Powershell"
-    $content.Save($testResultsFile)
+Import-Clixml -Path .\Test-Result.xml | Format-Pester -Format .\ -BaseFileName Test-Result -Format HTML -FailedOnly
+#Write-Host 'Completed Invoke-Pester. Saving results.'
+#    [xml]$content = Get-Content $testResultsFile
+#    $content.'test-results'.'test-suite'.type = "Powershell"
+#    $content.Save($testResultsFile)
 
-Write-Host 'Working with Format-Pester'
+#Write-Host 'Working with Format-Pester'
 
-if (Test-Path $testResultsFile) {
+#if (Test-Path $testResultsFile) {
 #  Import-Clixml -Path .\TestsResults.xml | Format-Pester -BaseFileName TestsResults -Format HTML,Word
 #Format-Pester -BaseFileName TestsResults -Format HTML,Word
-} else {
-  Write-Warning ("Testfile {0} not found!" -f $testResultsFile)
-      }
-      if ($res.FailedCount -gt 0) {
+#} else {
+#  Write-Warning ("Testfile {0} not found!" -f $testResultsFile)
+ #     }
+#      if ($res.FailedCount -gt 0) {
           # Terminate the script to fail the build
-          $Error | Format-List * -Force
-          exit 1
-      }
+#          $Error | Format-List * -Force
+#          exit 1
+#      }
