ba8602729821cd6c4181b2458d1186a032e0f751
Peter M Taylor <PeterMTaylor@users.noreply.github.com>
2017-07-08 14:51:05 +1000

    Update CodeAuditRD.ps1
    Put in functionality to go ahead and run the extract to we hope C:\Extract\ directory

--- CodeAuditRD.ps1
+++ CodeAuditRD.ps1
@@ -47,51 +47,51 @@ if ($Tags) {
 	 } # switch
 } # if $Tags
 
-#$SourceLocation="https://github.com/rubberduck-vba/Rubberduck/archive/ v2.0.13.zip"
-#  [string]$githubRepo = 
-#  [string]$destDir
-#$githubUriRegex="(?<Scheme>https://)(?<Host>github.com)/(?<User>\w*)/(?<Repo>\w*)(/archive/(?<Branch>master).zip)?"
-#
-#$githubMatch = [regex]::Match($githubRepo,$githubUriRegex)
-#
-#function GetGroupValue($match, [string]$group, [string]$default=""){
-#	$val=$match.Groups[$group].Value
+$SourceLocation="https://github.com/rubberduck-vba/Rubberduck/archive/v$Tags.zip"
+[string]$githubRepo = $SourceLocation
+[string]$destDir = "C:\Extract\"
+$githubUriRegex="(?<Scheme>https://)(?<Host>github.com)/(?<User>\w*)/(?<Repo>\w*)(/archive/(?<Branch>master).zip)?"
+$githubMatch = [regex]::Match($githubRepo,$githubUriRegex)
+
+function GetGroupValue($match, [string]$group, [string]$default=""){
+	$val=$match.Groups[$group].Value
 #	Write-Debug $val
-#	if($val){
-#		return $val
-#	}
-#	return $default
-#}
+	Write-Host $val	
+	if($val){
+		return $val
+	}
+	return $default
+}
 
-#if( ! $(GetGroupValue $githubMatch "Host") ){
-#	throw [System.ArgumentException] "Incorrect 'Host' value. The 'github.com' domain expected"
-#	#Write-Error -Message "Incorrect 'Host' value. The 'github.com' domain expected" -Category InvalidArgument
-#}
+if( ! $(GetGroupValue $githubMatch "Host") ){
+	throw [System.ArgumentException] "Incorrect 'Host' value. The 'github.com' domain expected"
+	#Write-Error -Message "Incorrect 'Host' value. The 'github.com' domain expected" -Category InvalidArgument
+}
 
-#Because url can be in different formats 
-#$githubUrl='https://github.com/{0}/{1}/archive/{2}.zip' -f 
-#						$(GetGroupValue $githubMatch "User"),
-#						$(GetGroupValue $githubMatch "Repo"), 
-#						$(GetGroupValue $githubMatch "Branch" "master")
+Because url can be in different formats 
+$githubUrl='https://github.com/{0}/{1}/archive/{2}.zip' -f 
+						$(GetGroupValue $githubMatch "User"),
+						$(GetGroupValue $githubMatch "Repo"), 
+						$(GetGroupValue $githubMatch "Branch" "master")
 
 
-#$file = [System.IO.Path]::GetTempFileName() + ".zip"
+$file = [System.IO.Path]::GetTempFileName() + ".zip"
 
-#New-Object System.Net.WebClient|%{$_.DownloadFile($githubUrl,$file);}
-#$sh = New-Object -ComObject Shell.Application
+New-Object System.Net.WebClient|%{$_.DownloadFile($githubUrl,$file);}
+$sh = New-Object -ComObject Shell.Application
 
-#$archive=$sh.NameSpace($file).Items();
+$archive=$sh.NameSpace($file).Items();
 
 #archive contain root folder. So get folder content
-#$archiveFilder=$archive.Item(0).GetFolder.Items()
+$archiveFilder=$archive.Item(0).GetFolder.Items()
 
-#if ( -Not $(Test-Path -path $destDir)){
-#	mkdir $destDir
-#}
-#else{
-#	Write-Warning "'$destDir' already exists."  -warningaction Inquire
-#}
+if ( -Not $(Test-Path -path $destDir)){
+	mkdir $destDir
+}
+else{
+	Write-Warning "'$destDir' already exists."  -warningaction Inquire
+}
 
-#$dst=$sh.NameSpace($destDir)
-#$dst.CopyHere($archiveFilder, 272)
+$dst=$sh.NameSpace($destDir)
+$dst.CopyHere($archiveFilder, 272)
 } # function CodeAuditRD 
